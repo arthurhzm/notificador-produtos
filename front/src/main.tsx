68236b97873c3fa7b4ac15d6b6263477f2ff0ba7
Arthur Marena <arthurzhm2@gmail.com>
2024-10-28 13:49:01 -0300

    FEAT: primeira página

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Login from './pages/page-login'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
