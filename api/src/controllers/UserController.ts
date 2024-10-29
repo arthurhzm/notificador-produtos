697bbb5fe1c56985605c610b7b97787edd9f4a74
Arthur Marena <arthurzhm2@gmail.com>
2024-10-29 18:12:00 -0300

    FEAT: criptografando senha e axios

--- api/src/controllers/UserController.ts
+++ api/src/controllers/UserController.ts
@@ -2,6 +2,7 @@ import { PrismaClient } from "@prisma/client";
 import { Request, Response } from "express";
 import HttpStatusCode from "../contants/HttpStatusCode";
 const prisma = new PrismaClient();
+const bcrypt = require('bcrypt');
 
 export const getUser = async (req: Request, res: Response): Promise<void> => {
     try {
@@ -16,7 +17,9 @@ export const getUser = async (req: Request, res: Response): Promise<void> => {
 export const createUser = async (req: Request, res: Response): Promise<void> => {
     try {
         const { name, email, password } = req.body;
-        const user = await prisma.user.create({ data: { name, email, password } });
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+        const user = await prisma.user.create({ data: { name, email, password: hashedPassword } });
         res.status(HttpStatusCode.CREATED).json({ user });
     } catch (error) {
         res.status(HttpStatusCode.INTERNAL_SERVER_ERROR).json({ error: 'Internal Server Error' });
