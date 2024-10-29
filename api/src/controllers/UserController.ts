f1763ca585dcc58b87af730e7a44b0abf853a46e
Arthur Marena <arthurzhm2@gmail.com>
2024-10-29 17:41:12 -0300

    FEAT: HttpStatusCodes

--- api/src/controllers/UserController.ts
+++ api/src/controllers/UserController.ts
@@ -1,9 +1,10 @@
 import { PrismaClient } from "@prisma/client";
 import { Request, Response } from "express";
+import HttpStatusCode from "../contants/HttpStatusCode";
 const prisma = new PrismaClient();
 
 export const getUser = async (req: Request, res: Response) => {
     const { id } = req.params;
     const user = await prisma.user.findUnique({ where: { id } });
-    return res.json(user);
+    return res.status(HttpStatusCode.OK).json(user);
 }
