ebce79228749352a635f21677d836f3ed902c7cd
Arthur Marena <arthurzhm2@gmail.com>
2024-10-29 07:05:38 -0300

    FEAT: formulário de login

--- front/src/pages/login-page.tsx
+++ front/src/pages/login-page.tsx
@@ -1,32 +1,59 @@
+import { z } from "zod";
 import Container from "../components/Container";
 import FormContainer from "../components/FormContainer";
 import InputText from "../components/InputText";
 import Main from "../components/Main";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
 
-export default function Login() {
+function LoginForm() {
 
-    const onSubmit = (data) => {
+    const schema = z.object({
+        email: z.string().email({ message: "E-mail inválido" }),
+        password: z.string().min(6, { message: "A senha deve ter pelo menos 6 caracteres" }),
+    });
+
+    type LoginFormType = z.infer<typeof schema>;
+
+    const { register, handleSubmit, formState } = useForm<LoginFormType>({
+        resolver: zodResolver(schema)
+    })
+
+    const onSubmit = (data: LoginFormType) => {
         console.log(data);
     }
 
     return (
+        <>
+            <h1>Bem-vindo de volta!</h1>
+            <FormContainer onSubmit={handleSubmit(onSubmit)}>
+                <InputText
+                    label="Email"
+                    type="email"
+                    {...register("email")}
+                    errors={formState.errors.email}
+                />
+                <InputText
+                    label="Senha"
+                    type="password"
+                    {...register("password")}
+                    errors={formState.errors.password}
+                />
+                <button type="submit">Entrar</button>
+            </FormContainer>
+            <div>
+                Não tem uma conta? <a href="/register">Cadastre-se</a>
+            </div>
+        </>
+    )
+
+}
+
+export default function Login() {
+    return (
         <Container>
             <Main>
-                <h1>Bem-vindo de volta!</h1>
-                <FormContainer onSubmit={onSubmit}>
-                    <InputText
-                        label="Email"
-                        type="email"
-                    />
-                    <InputText
-                        label="Senha"
-                        type="password"
-                    />
-                    <button type="submit">Entrar</button>
-                </FormContainer>
-                <div>
-                    Não tem uma conta? <a href="/register">Cadastre-se</a>
-                </div>
+                <LoginForm />
             </Main>
         </Container>
     )
